33e98dfb1623ffeb3a887c33e3560bb2f2b9bfe7
FifiBm <fifibm90@gmail.com>
2022-01-27 14:08:34 -0800

    This is  classAssignment  of  function that takes two parameters of string.

---
+++ classAssignment/index.js
@@ -0,0 +1,21 @@
+//**create a function named greet, that takes in two parameters: firstname and lastname
+//the output of greet should be a string ex.
+//greet("kayla", "golder")
+//Output: "hello, kayla golder. Its nice to meet you"
+
+
+const greet = (firstname , lastname) => {
+    return "hello," + " " + firstname + lastname + " " + "Its nice to meet you."
+}
+
+console.log (greet("Fifi","Bm"));
+
+
+
+
+
+
+
+
+
+
